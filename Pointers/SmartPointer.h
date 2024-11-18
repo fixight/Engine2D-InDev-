c7d889ebb417b1a38d2e370bfa923bf50781d62d
fixight <egorsmanev@gmail.com>
2024-11-18 23:11:07 +0300

    Render , Input , Base Classes , GameLoop , SmartPoiners

--- Pointers/SmartPointer.h
+++ Pointers/SmartPointer.h
@@ -5,55 +5,61 @@
 //���� ��� ������ ����� ����� ������� ��������� �� ������ , �� ��� ������������� ��� �� ������� ����� ������������ weak pointer
 
 template <typename T>
-class SmartPointer
-{
-
-	
-	SmartPointer& operator=(const SmartPointer& other) {
-		if (this != &other) {
-			release();
-			Pointer = other.Pointer;
-			RefCount = other.RefCount;
-			++(*RefCount);
-		}
-		return *this;
-	}
-
-
+class SmartPointer {
 public:
-	SmartPointer() : Pointer(nullptr), RefCount(new int(0)) {}
+    SmartPointer() : Pointer(nullptr), RefCount(new int(1)) {}
 
-	SmartPointer(T* ptr) : Pointer(ptr), RefCount(new int(1)) {}
+    
+    explicit SmartPointer(T* ptr) : Pointer(ptr), RefCount(new int(1)) {}
 
-	~SmartPointer() {
-		release();
-	}
+    
+    ~SmartPointer() {
+        release();
+    }
 
-	
-	SmartPointer(const SmartPointer& other) : Pointer(other.Pointer), RefCount(other.RefCount) {
-		++(*RefCount); 
-	}
+    
+    SmartPointer(const SmartPointer& other) 
+        : Pointer(other.Pointer), RefCount(other.RefCount) {
+        if (Pointer) {
+            ++(*RefCount);
+        }
+    }
 
+    
+    SmartPointer& operator=(const SmartPointer& other) {
+        if (this != &other) {
+            release();
+            Pointer = other.Pointer;
+            RefCount = other.RefCount;
+            if (Pointer) {
+                ++(*RefCount);
+            }
+        }
+        return *this;
+    }
+    
+    T* operator->() const {
+        return Pointer;
+    }
 
-
-
-
+    
+    T& operator*() const {
+        return *Pointer;
+    }
+    
+    T* Get() const {
+        return Pointer;
+    }
 
 private:
-	int* RefCount;
+    T* Pointer;      
+    int* RefCount;   
 
-	T* Pointer;
-
-	void release() {
-		if (--(*RefCount) == 0) {
-			delete Pointer;
-			delete RefCount;
-		}
-	}
-
-	
-
-
-
+    void release() {
+        if (RefCount && --(*RefCount) == 0) {
+            delete Pointer;   
+            delete RefCount;  
+        }
+    }
 };
 
